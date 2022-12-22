4de8cfc23647a5449bc0205ea6bb941f67ff2caa
LR <55665720+NancokPS2@users.noreply.github.com>
2022-12-22 14:12:40 -0300

    check availability function can return several val

--- Resources/Abilities/Abilities.gd
+++ Resources/Abilities/Abilities.gd
@@ -87,14 +87,15 @@ func _use(params):
 	pass
 	
 enum AvailabilityStatus {OK,CUSTOM_FALSE,NOT_ENOUGH_ENERGY,OTHER}
-func check_availability() -> int:
-	if _check_availability() == false:
-		return AvailabilityStatus.CUSTOM_FALSE
+func check_availability() -> int:  
+        var errorCode:int = 0
+	if _check_availability() == false:  
+		errorCode += AvailabilityStatus.CUSTOM_FALSE
 		
 	if user.stats["energy"] < energyCost:
-		return AvailabilityStatus.NOT_ENOUGH_ENERGY
+		errorCode += AvailabilityStatus.NOT_ENOUGH_ENERGY
 		
-	return AvailabilityStatus.OK
+	return errorCode
 
 func _check_availability() -> bool:#Virtual function, prevents usage if false
 	return true
