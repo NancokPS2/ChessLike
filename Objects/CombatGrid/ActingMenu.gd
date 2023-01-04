fba7487a93eab43beb85c1c218e0c7ce4d653e14
NancokPS2 <nancok@tuta.io>
2023-01-04 10:49:52 -0300

    Made ActionsMenu self sufficient

--- Objects/CombatGrid/ActingMenu.gd
+++ Objects/CombatGrid/ActingMenu.gd
@@ -2,14 +2,16 @@ extends VBoxContainerCollapsable
 
 func _ready() -> void:
 	._ready()#Run the signal connections
+	Events.connect("COMBAT_IDLE_enter",self,"expand",[true])
+	Events.connect("COMBAT_IDLE_exit",self,"expand",[false])
 
 func move_button():
 	Events.emit_signal("STATE_CHANGE_COMBAT",GameBoard.combatStates.MOVING)
+	expand(false)#Hide
 
 func act_button():
 	assert(Ref.unitInAction is Unit)
 	Events.emit_signal("STATE_CHANGE_COMBAT",GameBoard.combatStates.ACTING)
-	Events.emit_signal("COMBAT_ACTING_listabilities")
 	expand(false)#Hide
 	
 	
