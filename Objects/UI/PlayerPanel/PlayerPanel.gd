03401c62d596b434b507f9a5204f68710a86c9da
NancokPS2 <nancok@tuta.io>
2023-01-04 11:16:25 -0300

    Made InfoDisplay (class UnitDisplay) self sufficient

--- Objects/UI/PlayerPanel/PlayerPanel.gd
+++ Objects/UI/PlayerPanel/PlayerPanel.gd
@@ -1,5 +1,9 @@
 extends UnitDisplay
 
+func _ready() -> void:
+	Events.connect("GRID_UNIT_CLICKED",self,"load_unit")
+	Events.connect("GRID_UNIT_HOVERED",self,"load_unit")
+
 func refresh_ui():
 	if unitRef != null:
 		$Name.text = info["nickName"]
