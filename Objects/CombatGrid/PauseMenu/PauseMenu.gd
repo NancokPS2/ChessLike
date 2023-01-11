d78da2baa9aa6c8711f66e5728060d686f2a823c
NancokPS2 <nancok@tuta.io>
2023-01-11 10:07:12 -0300

    Pause menu now works properly (?)

--- Objects/CombatGrid/PauseMenu/PauseMenu.gd
+++ Objects/CombatGrid/PauseMenu/PauseMenu.gd
@@ -4,6 +4,8 @@ func _ready() -> void:
 	Events.connect("PAUSE_enter",self,"game_paused",[true])
 	Events.connect("PAUSE_exit",self,"game_paused",[false])
 	game_paused(false)
+	
+	$Buttons/Resume.connect("button_up",Events,"emit_signal",["STATE_CHANGE",GameBoard.states.COMBAT])
 
 func game_paused(yes:bool):
 	if yes:
@@ -12,3 +14,4 @@ func game_paused(yes:bool):
 		pause_mode = PAUSE_MODE_STOP
 		
 	visible = yes
+
