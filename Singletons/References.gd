3369104a73c67db765354e14514631f4e1dd9caa
NancokPS2 <nancok@tuta.io>
2023-06-11 16:33:52 -0300

    Implemented method to start the combat scene with a map to load.;

--- Singletons/References.gd
+++ Singletons/References.gd
@@ -1,7 +1,10 @@
 extends Node
  
-const COMBAT_SCENE:PackedScene = preload("res://Objects/Scenes/CombatField.tscn")
+const COMBAT_SCENE:PackedScene = preload("res://Objects/CombatGrid/CombatField.tscn")
 
+func _ready() -> void:
+#	start_combat(load("res://Resources/Maps/UniqueMaps/Default.tres") as Map)
+	pass
 
 var board:GameBoard
 var grid:MovementGrid #Combat field
@@ -31,5 +34,10 @@ var mainCamera:Node#Camera of the battlefield
 var debugRef
 
 func start_combat(map:Map):
+	get_tree().change_scene_to_packed(COMBAT_SCENE)
+	await get_tree().process_frame
+	var mainNode:GameBoard = get_tree().current_scene
+	assert(mainNode is GameBoard)
+	mainNode.load_map(map)
 	
 	pass
