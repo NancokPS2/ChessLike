40fe3d3eb845601620bad156f21ea25aa414991e
NancokPS2 <nancok@tuta.io>
2023-06-29 13:03:31 -0300

    Movement costs should be defined by the tags provided

--- Resources/Maps/MapTileSet.gd
+++ Resources/Maps/MapTileSet.gd
@@ -3,15 +3,20 @@ class_name MapTileSet
 
 @export var meshLibrary:MeshLibrary
 
+@export var tags:Dictionary = {0:["WALKABLE"],1:[""],2:[""],3:[""],4:[""],5:[""],6:[""],7:[""],8:[""],9:[""],10:[""],11:[""]} #cellID(int):CellTags(Array[String])
+#	set(val): 
+#		if val.size() > itemIDs.size():
+#			push_error("There's more arrays than cellIDs!")
+#			tags = []
+#		else:
+#			tags = val
+			
+
 var itemIDs:PackedInt32Array:
 	get: return meshLibrary.get_item_list()
 
-@export var moveCostWalk:Dictionary = {0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1}
-@export var moveCostFly:Dictionary = {0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1}
-@export var moveCostSwim:Dictionary = {0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1}
-
-func get_item_move_cost(itemID:int)->int:
-	var terrainCosts = moveCostWalk
-	if not itemIDs.has(itemID): push_error("itemIDs does not include " + str(itemID)); return 1
-	if not terrainCosts.has(itemID): push_error("terrainCosts does not include " + str(itemID)); return 1
-	return terrainCosts[itemID]
+#func get_item_move_cost(itemID:int)->int:
+#	var terrainCosts = moveCostWalk
+#	if not itemIDs.has(itemID): push_error("itemIDs does not include " + str(itemID)); return 1
+#	if not terrainCosts.has(itemID): push_error("terrainCosts does not include " + str(itemID)); return 1
+#	return terrainCosts[itemID]
