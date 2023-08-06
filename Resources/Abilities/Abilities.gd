9ebbfea8dc94847ceb7777daaee39ca3b98d343f
NancokPS2 <nancok@tuta.io>
2023-08-06 14:30:27 -0300

    Reworked how targetingShape works; Helper methods for the new shape system; queue_call now works again;

--- Resources/Abilities/Abilities.gd
+++ Resources/Abilities/Abilities.gd
@@ -32,7 +32,18 @@ enum AbilityFlags {
 
 enum AbilityTypes {MOVEMENT, OBJECT, SKILL, SPECIAL, PASSIVE}
 
-enum TargetingShapes {SELF, ANY, STAR, CONE, ALL}
+const TARGETING_SHAPE_FRONT:Array[Vector3i]=[Vector3i.FORWARD]
+const TARGETING_SHAPE_LINE_TWO:Array[Vector3i]=[Vector3i.FORWARD, Vector3i.FORWARD*2]
+const TARGETING_SHAPE_LINE_THREE:Array[Vector3i]=[Vector3i.FORWARD, Vector3i.FORWARD*2, Vector3i.FORWARD*3]
+const TARGETING_SHAPE_SELF:Array[Vector3i]=[Vector3i.ZERO]
+const TARGETING_SHAPE_ADJACENT:Array[Vector3i]=[Vector3i.LEFT, Vector3i.RIGHT, Vector3i.BACK, Vector3i.FORWARD]
+const TARGETING_SHAPE_ALL:Array[Vector3i]=[]
+const TARGETING_SHAPE_STAR_ONE:Array[Vector3i]=[Vector3i.ZERO, Vector3i.LEFT, Vector3i.RIGHT, Vector3i.BACK, Vector3i.FORWARD]
+const TARGETING_SHAPE_CONE_ONE:Array[Vector3i]=[Vector3i.FORWARD, Vector3i.FORWARD*2, Vector3i.FORWARD+Vector3i.LEFT, Vector3i.FORWARD+Vector3i.RIGHT]
+const TARGETING_SHAPE_BARRIER:Array[Vector3i]=[Vector3i.FORWARD, Vector3i.FORWARD+Vector3i.LEFT, Vector3i.FORWARD+Vector3i.RIGHT]
+
+
+static var callQueue:CallQueue
 
 var user:Unit:
 	set(val):
@@ -79,14 +90,11 @@ var miscOptions:Dictionary#Used to get extra parameters from the player
 @export var moveCost:int = 0
 
 @export_group("Targeting")
-@export var targetingShape:TargetingShapes
-@export var targetingAreaSize:int = 1 #Does nothing if the targetingShape does not support it
-@export var targetingRange:int#Distance in tiles from the player which it can target
+@export var targetingShape:Array[Vector3i] = TARGETING_SHAPE_ADJACENT #The area which the user can target
+@export var targetingAOEShape:Array[Vector3i] = TARGETING_SHAPE_SELF #The area relative to the targeted point that it will affect
+@export var targetingRotates:bool = false #If true, the targetingShape will be rotated to match the user's facing.
 
-@export var targetingAOEShape:TargetingShapes
-@export var targetingAOESize:int
-
-@export var amountOfTargets:int = 1
+@export var amountOfTargets:int = 1 #How many cells the user can target (the AOE will be applied to each one separately)
 @export var targetingFilterNames:Array[StringName] = ["has_unit"]:
 	set(val):
 #		assert(not val.is_empty())
@@ -118,11 +126,58 @@ func is_usable()->bool:
 	return true
 	pass
 
-func get_units(cells:Array[Vector3i])->Array[Unit]:
+func targeting_get_units_in_cells(cells:Array[Vector3i])->Array[Unit]:
 	var targets:Array[Unit] 
 	for cell in cells:
-		targets.assign(user.board.gridMap.search_in_tile(cell, MovementGrid.Searches.UNIT, true))
+		targets.assign(user.board.gridMap.search_in_cell(cell, MovementGrid.Searches.UNIT, true))
 	return targets
+	
+func targeting_get_relative_from_user(cellPos:Vector3i)->Vector3i:
+	var userCell:Vector3i = user.get_current_cell()
+	return cellPos - userCell
+#	var manhattanDistance:int = abs(userCell.x - cellPos.x) + abs(userCell.y - cellPos.y) + abs(userCell.z - cellPos.z)
+	
+
+func targeting_get_rotated_to_cell(shape:Array[Vector3i], targetCell:Vector3i)->Array[Vector3i]:
+	var relativeTarget:Vector3i = targeting_get_relative_from_user(targetCell)
+	var targetShapeHolder:Array[Vector3i] = shape.duplicate()
+	
+	#LEFT OR RIGHT is prioritized for now, this problem should be prevented by design.
+	if abs(relativeTarget.x) > abs(relativeTarget.z) or relativeTarget.x == relativeTarget.z:
+		#LEFT
+		if relativeTarget.sign().x == Vector3i.LEFT.x:
+#			direction = Vector3i.LEFT
+			for vec in targetingShape:
+				var newVec:Vector3i = vec
+				newVec.x = vec.z
+				newVec.z = -vec.x
+				targetShapeHolder.append(newVec)
+		#RIGHT
+		elif relativeTarget.sign().x == Vector3i.RIGHT.x:
+#			direction = Vector3i.RIGHT
+			for vec in targetingShape:
+				var newVec:Vector3i = vec
+				newVec.x = -vec.z
+				newVec.z = vec.x
+				targetShapeHolder.append(newVec)
+	else:
+		#BACK
+		if relativeTarget.sign().z == Vector3i.BACK.z:
+#			direction = Vector3i.BACK
+			for vec in targetingShape:
+				var newVec:Vector3i = vec
+				newVec.z = -vec.z
+				targetShapeHolder.append(newVec)
+			
+		elif relativeTarget.sign().z == Vector3i.FORWARD.z:
+#			direction = Vector3i.FORWARD
+			#Same as default, no rotation needed.
+			pass
+		
+		else: push_error("No direction could be stablished!")
+	
+	return targetShapeHolder
+	
 
 func connect_triggers():
 	if not user is Unit:#Ensure someone has equipped it
@@ -132,6 +187,7 @@ func connect_triggers():
 		var methodName:String = triggerSignals[signa]
 		var errorCode = connect(signa, Callable(self, methodName))
 		assert(errorCode == OK)
+
 
 #func filter_targetable_cells(cells:Array[Vector3i], shape:TargetingShapes=targetingShape)->Array[Vector3i]:
 #	var userCell:Vector3i = user.get_current_cell()
@@ -174,27 +230,25 @@ func connect_triggers():
 #	tween.tween_callback(use.bind(targets)).set_delay(0.2)
 #	tween.pause()
 #	return tween
-#func queue_call(targets:Array[Vector3i], queue:CallQueue):
-#	#Filter any unwanted targets.
-#	targets = filter_targets(targets)
-#	warn_units(targets)
-#
-#	#Create call
-#	queue.add_queued(use)
-#	queue.set_queued_arguments([targets])
-#	queue.set_queued_post_wait(animationDuration)
+func queue_call(targets:Array[Vector3i]):
+	#Filter any unwanted targets.
+	warn_units(targets)
+
+	#Create call
+	callQueue.add_queued(use)
+	callQueue.set_queued_arguments([targets])
+	callQueue.set_queued_post_wait(animationDuration)
 	
 ## Checks for units in the cells and warns them of an upcoming attack.
 func warn_units(targets:Array[Vector3i]):
 	var units:Array[Unit]
 	
 	for target in targets:
-		units.assign(board.gridMap.search_in_tile(target, MovementGrid.Searches.UNIT, true))
+		units.assign(board.gridMap.search_in_cell(target, MovementGrid.Searches.UNIT, true))
 		
 	for unit in units: unit.was_targeted.emit(self)
 	
-	
-func use( targets:Array[Vector3i] ):	
+func use( targets:Array[Vector3i] ):
 	user.attributes.stats.moves -= moveCost
 	user.attributes.stats.actions -= actionCost
 	user.attributes.stats.turnDelay += turnDelayCost
@@ -202,7 +256,7 @@ func use( targets:Array[Vector3i] ):
 #	targets = filter_targets(targets)
 	#The warning happens in get_tween(), no need for this.
 #	for target in targets:
-#		var possibleUnit:Unit = board.gridMap.search_in_tile(target, MovementGrid.Searches.UNIT)
+#		var possibleUnit:Unit = board.gridMap.search_in_cell(target, MovementGrid.Searches.UNIT)
 #		if possibleUnit is Unit:
 #			possibleUnit.was_targeted.emit(self)
 			
@@ -227,11 +281,11 @@ func _custom_filter(_target:Vector3i)->bool:
 class Filters extends RefCounted:
 	
 	#True if there's a unit there
-	static func has_unit(cell:Vector3i, _user:Unit): return true if Ref.grid.search_in_tile(cell,MovementGrid.Searches.UNIT) is Unit else false
+	static func has_unit(cell:Vector3i, _user:Unit): return true if Ref.grid.search_in_cell(cell,MovementGrid.Searches.UNIT) is Unit else false
 	#True if there's not a unit
-	static func not_has_unit(cell:Vector3i, _user:Unit): return false if Ref.grid.search_in_tile(cell,MovementGrid.Searches.UNIT) is Unit else true
+	static func not_has_unit(cell:Vector3i, _user:Unit): return false if Ref.grid.search_in_cell(cell,MovementGrid.Searches.UNIT) is Unit else true
 	#True if the tile has nothing in it
-	static func empty_tile(cell:Vector3i, _user:Unit): return true if Ref.grid.search_in_tile(cell,MovementGrid.Searches.ANYTHING) == null else false
+	static func empty_tile(cell:Vector3i, _user:Unit): return true if Ref.grid.search_in_cell(cell,MovementGrid.Searches.ANYTHING) == null else false
 	
 	static func is_ally(cell:Vector3i, user:Unit): 
 		var targetUnit:Unit = Ref.grid.search_int_tile(cell, MovementGrid.Searches.UNIT)
@@ -243,13 +297,13 @@ class Filters extends RefCounted:
 		else:
 			return false
 	
-	static func has_self(cell:Vector3i, user:Unit): return true if Ref.grid.search_in_tile(cell,MovementGrid.Searches.UNIT,true).has(user) else false
+	static func has_self(cell:Vector3i, user:Unit): return true if Ref.grid.search_in_cell(cell,MovementGrid.Searches.UNIT,true).has(user) else false
 	
-	static func not_has_self(cell:Vector3i, user:Unit): return false if Ref.grid.search_in_tile(cell,MovementGrid.Searches.UNIT,true).has(user) else true
+	static func not_has_self(cell:Vector3i, user:Unit): return false if Ref.grid.search_in_cell(cell,MovementGrid.Searches.UNIT,true).has(user) else true
 
 class Effects extends RefCounted:
 	
-	static func deal_damage(target:Unit, amount:float):
+	func deal_damage(targets:Array[Unit]):
 		
 		pass
 	pass
