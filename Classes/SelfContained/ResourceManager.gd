dd9d2a1da38c1feaba144e89ea17ba0c6d5c1493
NancokPS2 <nancok@tuta.io>
2023-08-18 14:07:32 -0300

    Rework

--- Classes/SelfContained/ResourceManager.gd
+++ Classes/SelfContained/ResourceManager.gd
@@ -2,6 +2,8 @@ extends Node
 class_name ResourceManager
 
 const cachePath:String = "user://cache/main.txt"
+const NO_IDENTIFIER:String = "_RESOURCEMANAGER_INTERNAL_NO_IDENTIFIER_ASSIGNED_"
+const ALL_POOLS:String = "_RESOURCEMANAGER_INTERNAL_ALL_POOLS_"
 
 #Folders whose contents will be automatically added when this node is ready
 @export var autoScanFolders:Dictionary = {} #Directory:group
@@ -15,8 +17,8 @@ const cachePath:String = "user://cache/main.txt"
 #		if autoLoadResources: val = true
 #		keepResourcesLoaded = true
 
-var resources:Dictionary = {
-}
+var resources:Dictionary
+var resourcesUnidentified:Dictionary
 
 var pools:Dictionary
 
@@ -40,7 +42,10 @@ func store_single_resource(filePath:String,group:String, keepLoaded:bool=keepLoa
 	
 	var identifier:String = _get_identifier(resLoaded)
 	
-	if identifier == "": push_error("Empty or invalid identifier. File path: {0} | Identifier: {1} | Group: {2}".format([filePath, identifier, group])); return
+	assert(identifier is String)
+	if identifier == NO_IDENTIFIER: 
+		push_warning("No identifier could be set, pooling in unidentified section. File path: {0} | Identifier: {1} | Group: {2}".format([filePath, identifier, group])) 
+		store_single_resource_unidentified(filePath, group, keepLoaded)
 	
 	#Ensure the group exists in resources
 	if not resources.has(group): resources[group] = {}
@@ -52,27 +57,31 @@ func store_single_resource(filePath:String,group:String, keepLoaded:bool=keepLoa
 		resources[group][identifier] = filePath
 	
 #	resources[group][fileName] = fileName
-	
-	
-func regenerate_cache():
-	var cache:=ConfigFile.new()
-	resources.clear()
+
+func store_single_resource_unidentified(filePath:String, group:String, keepLoaded:bool=keepLoadedDefault):	
+	if not resourcesUnidentified.has(group): resourcesUnidentified[group] = []
+	if keepLoaded:
+		resourcesUnidentified[group] = load(filePath)
+	else:
+		resourcesUnidentified[group] = filePath
+
+func reload_from_folders():
+#	var cache:=ConfigFile.new()
+	clear()
 	
 	for folder in autoScanFolders:
 		var group:String = autoScanFolders[folder]
 		store_from_folder(folder,group)
 	
-	
+## If check unidentified is true, a sweep will be made in an attempt to pull the resource from a group of unidentified resources.
+func assign_identifier(resource:Resource, identifier:String, checkUnidentified:bool, priorityGroup:String):
+	#TODO
 	pass
+	
+
 ## Attempts to retrieve a resource with the given identifier and keeps it loaded.
-func get_resource(identifier:String,group:String)->Resource:#If useCategory is true
-#	if identifier == "":
-#		push_error("No identifier given for " + group + " returnal.")
-#		return null
-#	elif group == "":
-#		push_error("Tried to retrieve resource but a group was not specified for file: " + identifier)
-#		return null
-		
+func get_resource(identifier:String,group:String)->Resource:
+
 	if resources.has(group) and resources[group].has(identifier):
 		
 		#If it has not been loaded yet, do so now.
@@ -84,18 +93,45 @@ func get_resource(identifier:String,group:String)->Resource:#If useCategory is t
 	else: 
 		push_error("Could not find resource with name '{0}' in group '{1}'".format([identifier,group]))
 		return null
-	
+
+func get_resources_unidentified(group:String)->Array:
+	if not resourcesUnidentified.has(group): push_error("There's no group with this name.")
+	return resourcesUnidentified.get(group,[])
+	pass
 
 ## Used when registering a Resource to the main Dictionary
 func _get_identifier(resource:Resource)->String:
 	return resource.resource_path
 
 	
-func add_to_pool(identifier:String, group:String, targetPool:String):
+func add_to_pool_by_identifier(identifier:String, group:String, targetPool:String):
 	var res:Resource = get_resource(identifier, group)
-	pools.get(targetPool,[]).append(res)
+	add_to_pool_by_resource(res, targetPool)
+	
+func add_to_pool_by_resource(resource:Resource, targetPool:String):
+	if not pools.has(targetPool): pools[targetPool] = []
+	
+	if targetPool == ALL_POOLS:
+		for pool in pools:
+			pools[pool].append(resource)
+	else:
+		pools[targetPool].append(resource)
+
+func remove_from_pool(resource:Resource, pool:String):
 	pass
 
-func get_all_in_group(group:String)->Array:
-	if not resources.has(group): push_error("The group {0} does not exist.".format([group])); return []
-	return resources[group].values()
+func get_all_in_group(group:String, includeUnidentified:bool = true)->Array:
+	if not resources.has(group): 
+		push_error("The group {0} does not exist.".format([group]))
+		return []
+	
+	var returned:Array
+	if includeUnidentified: returned.append_array(get_resources_unidentified(group))
+	
+	returned.append_array(resources[group].values())
+		
+	return returned
+
+func clear():
+	resources.clear()
+	resourcesUnidentified.clear()
