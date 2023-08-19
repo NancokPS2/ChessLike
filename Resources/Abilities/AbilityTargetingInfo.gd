a9d80c1625a55c95799755cfd0f6b1809604bac8
NancokPS2 <nancok@tuta.io>
2023-08-19 04:57:58 -0300

    Fail safes.

--- Resources/Abilities/AbilityTargetingInfo.gd
+++ Resources/Abilities/AbilityTargetingInfo.gd
@@ -1,10 +1,19 @@
 extends Resource
 class_name AbilityTargetingInfo
 
-var boardRef:GameBoard
-var gridRef:MovementGrid
+var boardRef:GameBoard:
+	get:
+		if not boardRef and user: push_warning("No boardRef was set, fetching from user."); return user.boardRef
+		else: return boardRef
+
+var gridRef:MovementGrid:
+	get:
+		if not gridRef and boardRef: push_warning("No gridRef was set, fetching from boardRef."); return boardRef.gridMap
+		else: return gridRef
+	
 
 var ability:Ability
 var user:Unit
+		
 var unitsTargeted:Array[Unit]
 var cellsTargeted:Array[Vector3i]
