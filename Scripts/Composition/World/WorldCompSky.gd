636d15c808c2e143092a11b5344c46c439f40558
NancokPS2 <nancok@tuta.io>
2024-05-10 10:57:22 -0300

    World comps2 ammend

--- Scripts/Composition/World/WorldCompSky.gd
+++ Scripts/Composition/World/WorldCompSky.gd
@@ -5,9 +5,9 @@ const COMPONENT_NAME: StringName = "WORLD_SKY"
 
 ## Minutes in a day
 const SunColors: Dictionary = {
-	MORNING = Color(1,0.612,0.275),
-	DAY = Color(1,1,0.671),
-	NIGHT = Color(1,1,0.671),
+	MORNING = Color8(255,221,64),
+	DAY = Color8(252,255,181),
+	NIGHT = Color8(194,197,204),
 }
 const SUN_COLOR_DAY := Color(1,1,0.671)
 const SUN_COLOR_NOON := Color(1,0.612,0.275)
@@ -15,16 +15,16 @@ const SUN_COLOR_NOON := Color(1,0.612,0.275)
 
 var sunlight_node := DirectionalLight3D.new()
 
-var sun_color: Color
+var custom_sun_color := Color.TRANSPARENT
 
 var sun_tilt: float = 0
 
 func _ready() -> void:
 	assert(get_parent() is WorldNode3D)
 	
-	await get_tree().process_frame
-	add_child(sunlight_node)
-	update_sun_position()
+	add_child.call_deferred(sunlight_node)
+	update_sun_position.call_deferred()
+	print.call_deferred(sunlight_node.get_parent())
 
 
 func _process(_delta):
@@ -53,24 +53,35 @@ func get_daytime() -> float:
 		return 0
 	else:
 		assert(calendar_comp.get_time_frame(calendar_comp.Lengths.DAY)<calendar_comp.Lengths.DAY)
-		return calendar_comp.get_time_frame(calendar_comp.Lengths.DAY)
+		var time_this_day: float = calendar_comp.get_time_frame(calendar_comp.Lengths.DAY)
+		var offset: float = calendar_comp.Lengths.DAY_QUARTER
+		var output: float = remap(time_this_day + offset,
+			0 + offset,
+			calendar_comp.Lengths.DAY + offset,
+			0,
+			calendar_comp.Lengths.DAY
+			)
+		return output
 
 
 func update_sun_position(day_time: float = get_daytime()):
 	var rise: float = remap(day_time, 0, WorldCompTime.Lengths.DAY, 0, TAU)
 	var tilt: float = sun_tilt
 	sunlight_node.rotation = Vector3(rise, 0, tilt)
-	print_debug(sunlight_node.rotation)
 
 
 func update_sun_color(day_time: float = get_daytime()):
-	const QUARTER_DAY: float = WorldCompTime.Lengths.DAY_QUARTER
+	if custom_sun_color != Color.TRANSPARENT:
+		sunlight_node.light_color = custom_sun_color
+		return 
+		
+	var quarter: float = WorldCompTime.Lengths.DAY_QUARTER
 	
 	var starter_color: Color
 	var final_color: Color
 	
-	var curr_quarter: float = floor(day_time / QUARTER_DAY)
-	var prog_quarter: float = fmod(day_time, QUARTER_DAY)
+	var curr_quarter: int = floori(day_time / quarter)
+	var prog_quarter: float = fmod(day_time, quarter)/360
 	
 	match curr_quarter:
 		0:
@@ -87,5 +98,8 @@ func update_sun_color(day_time: float = get_daytime()):
 			final_color = SunColors.NIGHT
 			
 	var color: Color = starter_color.lerp(final_color, prog_quarter)
-	
+	print_debug('''Start: {1}
+	Final: {2}
+	Output: {0}'''.format([str(color), str(starter_color), str(final_color)]))
+	print(SunColors.NIGHT)
 	sunlight_node.light_color = color
